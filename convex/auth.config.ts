7cf4f492727cc96b48508b1b9b6f52e59d207d01
marlon ureta <fabrisio021@gmail.com>
2025-03-01 11:04:30 -0500

    added header main page

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+	providers: [
+		{
+			domain: 'https://actual-koala-17.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+};
+
+export default authConfig;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./_components/Header";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <div className="max-w-[1800px] mx-auto p-4">
+        <Header />
+
+        
+      </div>
+    </div>
+  );
+}
