2155888064209fc9e6e47150dd21cd803b9cd490
marlon ureta <fabrisio021@gmail.com>
2025-03-01 22:24:14 -0500

    added skeletons snippet

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -10,8 +10,6 @@ import { useClerk } from '@clerk/nextjs';
 import { EditorPanelSkeleton } from './EditorPanelSkeleton';
 import useMounted from '@/hooks/useMounted';
 import ShareSnippetDialog from './ShareSnippetDialog';
-// import ShareSnippetDialog from './ShareSnippetDialog';
-
 function EditorPanel() {
 	const clerk = useClerk();
 	const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
